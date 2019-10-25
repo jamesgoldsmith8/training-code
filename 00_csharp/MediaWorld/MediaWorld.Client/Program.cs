89475f081527d3141de40e3102479694a104cef9
James Goldsmith <jamesg.developer@gmail.com>
2019-10-25 09:33:42 -0500

    add MediaWorld

---
+++ 00_csharp/MediaWorld/MediaWorld.Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MediaWorld.Domain.Models;
+
+namespace MediaWorld.Client
+{
+    /// <summary>
+    /// contain the start point
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// starts the application
+        /// </summary>
+        private static void Main()
+        {
+            var helper = MediaSingleton.GetInstance();
+            Console.WriteLine(helper);
+        }
+    }
+}
