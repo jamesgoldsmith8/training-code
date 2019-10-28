59747d8f350f97b3f6dc64b4f69d9b50e0995198
James Goldsmith <jamesg.developer@gmail.com>
2019-10-28 16:00:28 -0500

    add Fred changes

--- 00_csharp/MediaWorld/MediaWorld.Client/Program.cs
+++ 00_csharp/MediaWorld/MediaWorld.Client/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using MediaWorld.Domain.Abstracts;
 using MediaWorld.Domain.Models;
+using MediaWorld.Domain.Singletons;
 
 namespace MediaWorld.Client
 {
@@ -18,12 +20,22 @@ namespace MediaWorld.Client
 
         private static void Play()
         {
+            /*
             var mediaPlayer = MediaSingleton.GetInstance();
+             
             Music song = new Song();
             Music audible = new Audible();
 
             mediaPlayer.Play(song);
             mediaPlayer.Play(audible);
+            */
+
+            var mediaPlayer = MediaSingleton.Instance;
+            AMedia song = new Song();
+            AMedia movie = new Movie();
+
+            mediaPlayer.Execute("play", song);
+            mediaPlayer.Execute("play", movie);
         }
     }
 }
---
+++ 00_csharp/MediaWorld/MediaWorld.Domain/Singletons/MediaSingleton.cs
@@ -0,0 +1,35 @@
+using System;
+using MediaWorld.Domain.Abstracts;
+using MediaWorld.Domain.Interfaces;
+
+namespace MediaWorld.Domain.Singletons
+{
+    public class MediaSingleton : IPlayer
+    {
+        private static readonly MediaSingleton _instance = new MediaSingleton();
+
+        private MediaSingleton() {}
+/*        public static MediaSingleton GetInstance() 
+        {
+            return _instance;
+        }
+
+        public void Play(Music m)
+        {
+            Console.WriteLine(m);
+        }
+*/
+        public static MediaSingleton Instance 
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+        
+        public void Execute(string command, AMedia media) 
+        {
+            Console.WriteLine(media);
+        }
+    }
+}
